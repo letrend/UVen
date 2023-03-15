e923e77a2dc3a7e4d2fab8364bde5d7fd3bbabd6
letrend <simontrendel@gmail.com>
2023-03-15 21:55:07 +0100

    with tft display working and showing temperature

--- src/uven_pocket/uven_pocket.ino
+++ src/uven_pocket/uven_pocket.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>             // Arduino SPI library
 #include <Encoder.h>
@@ -15,8 +16,8 @@
 #define BUTTON_1 40
 #define BUTTON_2 39
 
-// #define TFT_DC    36  // define data/command pin
-// #define TFT_RST   35  // define reset pin, or set to -1 and connect to Arduino RESET pin
+ #define TFT_DC    36  // define data/command pin
+ #define TFT_RST   35  // define reset pin, or set to -1 and connect to Arduino RESET pin
 #define LED_0_DISABLE 14
 #define LED_1_DISABLE 15
 
@@ -25,9 +26,11 @@
 #define TEC_LED1_IN 3
 #define TEC_LED1_STATUS 6
 
+#define TEC_LEDS 28
+
 bool led_fire = false;
  
-// Adafruit_ST7789 tft = Adafruit_ST7789(-1, TFT_DC, TFT_RST);
+ Adafruit_ST7789 tft = Adafruit_ST7789(-1, TFT_DC, TFT_RST);
 SevSeg sevseg; 
 int encoder_pos = 0;
 int poti_setpoint = -1;
@@ -38,7 +41,7 @@ Encoder myEnc(ENCODER_A, ENCODER_B);
 ros::NodeHandle nh;
 std_msgs::Float32 temp[3];
 ros::Publisher temp_pub[3]= { ros::Publisher("temp0", &temp[0]), ros::Publisher("temp1", &temp[1]), ros::Publisher("temp2", &temp[2])};
-unsigned long t0,t1;
+unsigned long t0,t1,t2;
 float poly[3][3] = { {8.57991735e-05, -2.05112717e-01,  1.05402649e+02},
 {1.29957085e-04, -2.37834294e-01,  1.09390237e+02},
 {1.30629020e-04, -2.35951069e-01,  1.08283965e+02} };
@@ -50,13 +53,22 @@ float calcTemp(int val, float *p){
   return p[0]*val*val+p[1]*val+p[2];
 }
 
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
 void setup() {
   // Serial.begin(115200);
   // pinMode(18,OUTPUT);
   // digitalWrite(18,HIGH);
-  // tft.init(240, 240, SPI_MODE2);
-  // tft.setRotation(2);
-  // tft.fillScreen(ST77XX_BLUE);
+   tft.init(240, 240, SPI_MODE2);
+   tft.setRotation(2);
+   tft.fillScreen(ST77XX_BLACK);
+   tft.setTextColor(ST77XX_WHITE);
+   tft.setTextSize(6);
   // pinMode(ENCODER_BUTTON,INPUT_PULLUP);
 
   Wire.begin();
@@ -84,6 +96,14 @@ void setup() {
   pinMode(TEC_LED0_IN,OUTPUT);
   pinMode(TEC_LED1_IN,OUTPUT);
 
+  pinMode(TEC_LEDS,OUTPUT);
+  for(int i=0;i<3;i++){
+    digitalWrite(TEC_LEDS,true);
+    delay(100);
+    digitalWrite(TEC_LEDS,false);
+    delay(100);
+  }
+
   byte numDigits = 4;  
   byte digitPins[] = {50, 48, 52, 53};
   byte segmentPins[] = {42, 51, 44, 46, 45, 47, 49, 43};
@@ -98,8 +118,6 @@ void setup() {
 
 void loop() {
   encoder_pos = myEnc.read();
-  // tft.fillScreen(ST77XX_BLUE);
-  // testdrawtext(distance, ST77XX_WHITE,6,"penis");
   // Serial.println(distance);
   if(encoder_pos>=0 && encoder_pos<100){
     if(poti_setpoint!=encoder_pos && !led_fire){
@@ -128,8 +146,6 @@ void loop() {
   
   t1 = millis();
 
-  
-  
   if(t1-t0>100){
     for(int i=0;i<3;i++){
       analogValue[i] = analogRead(analogPin[i]);  
@@ -139,15 +155,17 @@ void loop() {
     t0 = t1;
     nh.spinOnce();
 
-    if(temp[0].data>temp_setpoint){
-      digitalWrite(TEC_LED1_IN,1);
+    if(temp[0].data>temp_setpoint || temp[1].data>temp_setpoint){
+      digitalWrite(TEC_LEDS,true);
     }else{
-      digitalWrite(TEC_LED1_IN,0);
+      digitalWrite(TEC_LEDS,false);
     }
-    if(temp[1].data>temp_setpoint){
-      digitalWrite(TEC_LED0_IN,1);
+    if(temp[2].data>temp_setpoint){
+      digitalWrite(TEC_LED0_IN,true);
+      digitalWrite(TEC_LED1_IN,true);
     }else{
-      digitalWrite(TEC_LED0_IN,0);
+      digitalWrite(TEC_LED0_IN,false);
+      digitalWrite(TEC_LED1_IN,false);
     }
 //    if(temp[0].data>temp_setpoint){
 //      float error = temp[0].data-temp_setpoint;
@@ -169,5 +187,36 @@ void loop() {
 //    }else{
 //      analogWrite(TEC_LED1_IN,0);
 //    }
+  }
+  if(t1-t2>1000){
+    t2 = t1;
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    if(temp[0].data > temp_setpoint){
+      tft.setTextColor(ST77XX_YELLOW);
+    }else if(temp[0].data > 60){
+      tft.setTextColor(ST77XX_RED);
+    }else{
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(temp[0].data);
+    tft.setCursor(0, 60);
+    if(temp[1].data > temp_setpoint){
+      tft.setTextColor(ST77XX_YELLOW);
+    }else if(temp[1].data > 60){
+      tft.setTextColor(ST77XX_RED);
+    }else{
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(temp[1].data);
+    tft.setCursor(0, 120);
+    if(temp[2].data > temp_setpoint){
+      tft.setTextColor(ST77XX_YELLOW);
+    }else if(temp[2].data > 60){
+      tft.setTextColor(ST77XX_RED);
+    }else{
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(temp[2].data);
   }
 }
