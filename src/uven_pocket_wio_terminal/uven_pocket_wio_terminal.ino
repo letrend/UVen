82eb5cd864cb620cbb1126ad18f6d2aa1458bb0a
letrend <simontrendel@gmail.com>
2023-04-08 13:24:01 +0200

    with setpoints

--- src/uven_pocket_wio_terminal/uven_pocket_wio_terminal.ino
+++ src/uven_pocket_wio_terminal/uven_pocket_wio_terminal.ino
@@ -3,14 +3,18 @@
 Arduino_CRC32 crc32;
 #include "seeed_line_chart.h" //include the library
 
-TFT_eSPI tft;
+#include "analogMeter.h"
+TFT_eSprite analogMeter_spr = TFT_eSprite(&tft); // Sprite
+TFT_eSprite analogMeter_spr2 = TFT_eSprite(&tft);
 TFT_eSprite spr0 = TFT_eSprite(&tft); // Sprite
 TFT_eSprite spr1 = TFT_eSprite(&tft); // Sprite
 TFT_eSprite spr2 = TFT_eSprite(&tft); // Sprite
 #include"Free_Fonts.h" //include the header file
 
+#define LOOP_PERIOD 35 // Display updates every 35 ms
 #define MAX_SIZE 30 // maximum size of data
 doubles temp[3];       // Initilising a doubles type to store data
+doubles inten[2];
 
 #define CS 1 
 #define RESET_COMS 0
@@ -39,11 +43,15 @@ float temp_min[3] = {1000,1000,1000};
 
 enum {
   TEMPERATURE,
-  SETPOINTS,
+  SETPOINT_TEMP_0,
+  SETPOINT_TEMP_1,
+  SETPOINT_TEMP_2,
+  SETPOINT_LED_0,
+  SETPOINT_LED_1,
   CRC_MISMATCH
 };
 
-uint8_t menu = TEMPERATURE;
+int8_t menu = TEMPERATURE, menu_prev = TEMPERATURE;
 
 void setup() {
   pinMode(CS, OUTPUT);
@@ -68,6 +76,8 @@ void setup() {
   t0 = millis();
   tft.fillScreen(TFT_WHITE); 
 
+  analogMeter_spr.createSprite(TFT_HEIGHT,TFT_WIDTH/3-15);
+
   pinMode(WIO_KEY_A, INPUT_PULLUP);
   pinMode(WIO_KEY_B, INPUT_PULLUP);
   pinMode(WIO_KEY_C, INPUT_PULLUP);
@@ -77,11 +87,6 @@ void setup() {
   pinMode(WIO_5S_LEFT, INPUT_PULLUP);
   pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
   pinMode(WIO_5S_PRESS, INPUT_PULLUP);
-}
-
-void loop() {
-  digitalWrite(CS, LOW);
-  delay(10);
 
   cmd.values.control[0] = 0;
   cmd.values.control[1] = 0;
@@ -91,6 +96,12 @@ void loop() {
   cmd.values.temperature[0] = 24;
   cmd.values.temperature[1] = 24;
   cmd.values.temperature[2] = 24;
+}
+
+void loop() {
+  digitalWrite(CS, LOW);
+  delay(10);
+
   cmd.values.crc = crc32.calc((uint8_t const *)&cmd.data[0], 20);
 
   for(int i=0;i<BUFFER_SIZE;i++){
@@ -125,12 +136,41 @@ void loop() {
 //    Serial.print("\t");
 //    Serial.println(res.values.temperature[2]);
     t1 = millis();
-    if(WIO_KEY_A==0){
+    if(digitalRead(WIO_KEY_A)==0){
+      menu--;
+    }
+    if(digitalRead(WIO_KEY_C)==0){
+      menu++;
+    }
+
+    if(menu>5){
       menu = TEMPERATURE;
     }
-    if(WIO_KEY_C==0){
-      menu = SETPOINTS;
+    if(menu<0){
+      menu = SETPOINT_LED_1;
     }
+
+    if(menu!=menu_prev){
+      tft.fillScreen(TFT_WHITE); 
+      if(menu>TEMPERATURE){
+        analogMeter(); // Draw analogue meter
+      }
+      menu_prev = menu;
+    }
+
+    if (temp[0].size() > MAX_SIZE)
+    {
+        temp[0].pop(); 
+        temp[1].pop();
+        temp[2].pop();
+        inten[0].pop();
+        inten[1].pop();
+    }
+    temp[0].push(res.values.temperature[0]);
+    temp[1].push(res.values.temperature[1]);
+    temp[2].push(res.values.temperature[2]);
+    inten[0].push(cmd.values.intensity[0]);
+    inten[1].push(cmd.values.intensity[1]);
     
     switch(menu){
       case TEMPERATURE:
@@ -139,15 +179,6 @@ void loop() {
           spr0.fillSprite(TFT_WHITE);
           spr1.fillSprite(TFT_WHITE);
           spr2.fillSprite(TFT_WHITE);
-          if (temp[0].size() > MAX_SIZE)
-          {
-              temp[0].pop(); 
-              temp[1].pop();
-              temp[2].pop();
-          }
-          temp[0].push(res.values.temperature[0]);
-          temp[1].push(res.values.temperature[1]);
-          temp[2].push(res.values.temperature[2]);
     
           if(res.values.temperature[0]<temp_min[0]){
             temp_min[0] = res.values.temperature[0];
@@ -236,9 +267,248 @@ void loop() {
           spr2.pushSprite(5, spr0.height()*2+header0.height()*3);
         }
         break;
-      case SETPOINTS:
+      case SETPOINT_TEMP_0:{
+        // Settings for the line graph title
+          auto header0 = text(0, 0)
+                            .value("LED0 TEMPERATURE")
+                            .align(left)
+                            .valign(vcenter)
+                            .width(tft.width())
+                            .thickness(2);
+      
+          header0.height(header0.font_height(&tft));
+          header0.draw(&tft); // Header height is the twice the height of the font
+          
+         analogMeter_spr.fillSprite(TFT_WHITE);
+         auto content = line_chart(8, 0); //(x,y) where the line graph begins
+               content
+                      .height(tft.height() ) //actual height of the line chart
+                      .width(tft.width()) //actual width of the line chart
+                      .based_on(0.0) //Starting point of y-axis, must be a float
+                      .show_circle(true) //drawing a cirle at each point, default is on.
+                      .value(temp[0]) //passing through the data to line graph
+                      .color(TFT_PURPLE) //Setting the color for the line
+                      .draw(&analogMeter_spr2);
+                       
+          analogMeter_spr.pushSprite(0, 190);
+
+          char str[10];
+          sprintf(str,"setpoint: %.1f",cmd.values.temperature[0]);
+          tft.drawCentreString(str, 160, 140, 2);
+          if(digitalRead(WIO_5S_UP)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[0]+=0.5;
+          }else if(digitalRead(WIO_5S_DOWN)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[0]-=0.5;
+          }
+      
+          if (updateTime <= millis()) {
+              updateTime = millis() + LOOP_PERIOD;
+              plotNeedle(temp[0].back(), 0);
+          }
+          break;
+      }
+     case SETPOINT_TEMP_1:{
+      // Settings for the line graph title
+          auto header0 = text(0, 0)
+                            .value("LED1 TEMPERATURE")
+                            .align(left)
+                            .valign(vcenter)
+                            .width(tft.width())
+                            .thickness(2);
+      
+          header0.height(header0.font_height(&tft));
+          header0.draw(&tft); // Header height is the twice the height of the font
+          analogMeter_spr.fillSprite(TFT_WHITE);
+          auto content = line_chart(8, 0); //(x,y) where the line graph begins
+               content
+                      .height(tft.height() ) //actual height of the line chart
+                      .width(tft.width()) //actual width of the line chart
+                      .based_on(0.0) //Starting point of y-axis, must be a float
+                      .show_circle(true) //drawing a cirle at each point, default is on.
+                      .value(temp[1]) //passing through the data to line graph
+                      .color(TFT_PURPLE) //Setting the color for the line
+                      .draw(&analogMeter_spr2);
+                       
+          analogMeter_spr.pushSprite(0, 190);
+
+          char str[10];
+          sprintf(str,"setpoint: %.1f",cmd.values.temperature[1]);
+          tft.drawCentreString(str, 160, 140, 2);
+          if(digitalRead(WIO_5S_UP)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[1]+=0.5;
+          }else if(digitalRead(WIO_5S_DOWN)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[1]-=0.5;
+          }
+          
+          if (updateTime <= millis()) {
+              updateTime = millis() + LOOP_PERIOD;
+              plotNeedle(temp[1].back(), 0);
+          }
+          break;
+       }
+       case SETPOINT_TEMP_2:{
+        // Settings for the line graph title
+          auto header0 = text(0, 0)
+                            .value("CHAMBER TEMPERATURE")
+                            .align(left)
+                            .valign(vcenter)
+                            .width(tft.width())
+                            .thickness(2);
+      
+          header0.height(header0.font_height(&tft));
+          header0.draw(&tft); // Header height is the twice the height of the font
         
-        break;
+         analogMeter_spr.fillSprite(TFT_WHITE);
+         auto content = line_chart(8, 0); //(x,y) where the line graph begins
+               content
+                      .height(tft.height() ) //actual height of the line chart
+                      .width(tft.width()) //actual width of the line chart
+                      .based_on(0.0) //Starting point of y-axis, must be a float
+                      .show_circle(true) //drawing a cirle at each point, default is on.
+                      .value(temp[2]) //passing through the data to line graph
+                      .color(TFT_PURPLE) //Setting the color for the line
+                      .draw(&analogMeter_spr2);
+                       
+          analogMeter_spr.pushSprite(0, 190);
+
+          char str[10];
+          sprintf(str,"setpoint: %.1f",cmd.values.temperature[2]);
+          tft.drawCentreString(str, 160, 140, 2);
+          if(digitalRead(WIO_5S_UP)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[2]+=0.5;
+          }else if(digitalRead(WIO_5S_DOWN)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.temperature[2]-=0.5;
+          }
+      
+          if (updateTime <= millis()) {
+              updateTime = millis() + LOOP_PERIOD;
+              plotNeedle(temp[2].back(), 0);
+          }
+          break;
+       }
+       case SETPOINT_LED_0:{
+        // Settings for the line graph title
+          auto header0 = text(0, 0)
+                            .value("LED0 INTENSITY")
+                            .align(left)
+                            .valign(vcenter)
+                            .width(tft.width())
+                            .thickness(2);
+      
+          header0.height(header0.font_height(&tft));
+          header0.draw(&tft); // Header height is the twice the height of the font
+          
+         analogMeter_spr.fillSprite(TFT_WHITE);
+         auto content = line_chart(8, 0); //(x,y) where the line graph begins
+               content
+                      .height(tft.height() ) //actual height of the line chart
+                      .width(tft.width()) //actual width of the line chart
+                      .based_on(0.0) //Starting point of y-axis, must be a float
+                      .show_circle(true) //drawing a cirle at each point, default is on.
+                      .value(inten[0]) //passing through the data to line graph
+                      .color(TFT_PURPLE) //Setting the color for the line
+                      .draw(&analogMeter_spr2);
+                       
+          analogMeter_spr.pushSprite(0, 190);
+
+          char str[10];
+          sprintf(str,"setpoint: %d",cmd.values.intensity[0]);
+          tft.drawCentreString(str, 160, 140, 2);
+          if(digitalRead(WIO_5S_UP)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.intensity[0]+=5;
+            if(cmd.values.intensity[0]>=100){
+              cmd.values.intensity[0]=100;
+            }
+          }else if(digitalRead(WIO_5S_DOWN)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.intensity[0]-=5;
+            if(cmd.values.intensity[0]<5){
+              cmd.values.intensity[0]=5;
+            }
+          }
+
+          if (updateTime <= millis()) {
+              updateTime = millis() + LOOP_PERIOD;
+              plotNeedle(inten[0].back(), 0);
+          }
+     
+          break;
+      }
+     case SETPOINT_LED_1:{
+        // Settings for the line graph title
+          auto header0 = text(0, 0)
+                            .value("LED1 INTENSITY")
+                            .align(left)
+                            .valign(vcenter)
+                            .width(tft.width())
+                            .thickness(2);
+      
+          header0.height(header0.font_height(&tft));
+          header0.draw(&tft); // Header height is the twice the height of the font
+          
+         analogMeter_spr.fillSprite(TFT_WHITE);
+         auto content = line_chart(8, 0); //(x,y) where the line graph begins
+               content
+                      .height(tft.height() ) //actual height of the line chart
+                      .width(tft.width()) //actual width of the line chart
+                      .based_on(0.0) //Starting point of y-axis, must be a float
+                      .show_circle(true) //drawing a cirle at each point, default is on.
+                      .value(inten[1]) //passing through the data to line graph
+                      .color(TFT_PURPLE) //Setting the color for the line
+                      .draw(&analogMeter_spr2);
+                       
+          analogMeter_spr.pushSprite(0, 190);
+
+          char str[10];
+          sprintf(str,"setpoint: %d",cmd.values.intensity[1]);
+          tft.drawCentreString(str, 160, 140, 2);
+          if(digitalRead(WIO_5S_UP)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.intensity[1]+=5;
+            if(cmd.values.intensity[1]>=100){
+              cmd.values.intensity[1]=100;
+            }
+          }else if(digitalRead(WIO_5S_DOWN)==0){
+            tft.setTextColor(TFT_WHITE, TFT_WHITE);
+            tft.drawCentreString(str, 160, 140, 2);
+            tft.setTextColor(TFT_BLACK, TFT_WHITE);
+            cmd.values.intensity[1]-=5;
+            if(cmd.values.intensity[1]<5){
+              cmd.values.intensity[1]=5;
+            }
+          }
+          if (updateTime <= millis()) {
+              updateTime = millis() + LOOP_PERIOD;
+              plotNeedle(inten[1].back(), 0);
+          }
+     
+          break;
+      }
     }
     
     
