b969fb0e9dcc3fa92cd0e436ce0141d8baa8fa17
letrend <simontrendel@gmail.com>
2023-08-16 18:19:41 +0200

    gui looks good

--- src/uven2_mk2_wio_terminal_guislicer/uven2_mk2_wio_terminal_guislicer.ino
+++ src/uven2_mk2_wio_terminal_guislicer/uven2_mk2_wio_terminal_guislicer.ino
@@ -13,7 +13,13 @@
 // Headers to include
 // ------------------------------------------------
 #include "uven2_mk2_wio_terminal_guislicer_GSLC.h"
+#include <Arduino_CRC32.h>
+#include <EasyButton.h>
+Arduino_CRC32 crc32;
+#include "seeed_line_chart.h"
+TFT_eSPI tft;
 
+#define BUFFER_SIZE 176
 
 // ------------------------------------------------
 // Program Globals
@@ -21,44 +27,242 @@
 
 // Save some element references for direct access
 //<Save_References !Start!>
-gslc_tsElemRef* m_pElemProgress1  = NULL;
+gslc_tsElemRef* m_chamberFan      = NULL;
+gslc_tsElemRef* m_drvTemp         = NULL;
+gslc_tsElemRef* m_drvTempSlider   = NULL;
+gslc_tsElemRef* m_intensitySlider = NULL;
+gslc_tsElemRef* m_interlock       = NULL;
+gslc_tsElemRef* m_ledTemp         = NULL;
+gslc_tsElemRef* m_ledTempSlider   = NULL;
+gslc_tsElemRef* m_rep             = NULL;
+gslc_tsElemRef* m_repEnable       = NULL;
+gslc_tsElemRef* m_repSec          = NULL;
+gslc_tsElemRef* m_slider          = NULL;
+gslc_tsElemRef* m_timeHour        = NULL;
+gslc_tsElemRef* m_timeMin         = NULL;
+gslc_tsElemRef* m_timeSec         = NULL;
+gslc_tsElemRef* m_pElemKeyPadNum  = NULL;
 //<Save_References !End!>
 
-// Define debug message function
-static int16_t DebugOut(char ch) { if (ch == (char)'\n') Serial.println(""); else Serial.write(ch); return 0; }
+union SERIAL_FRAME{
+  struct{
+    uint32_t time;
+    float temperature[17];
+    uint16_t led_fan;
+    uint16_t chamber_fan;
+    uint16_t target_current[16];
+    uint16_t current[16];
+    uint16_t gate[16];
+    uint32_t crc;
+  }values;
+  volatile uint8_t data[BUFFER_SIZE];
+};
+
+SERIAL_FRAME rx, tx;
+
+EasyButton but_a(WIO_KEY_A), but_b(WIO_KEY_B), but_c(WIO_KEY_C);
+EasyButton but_up(WIO_5S_UP), but_down(WIO_5S_DOWN), but_left(WIO_5S_LEFT), but_right(WIO_5S_RIGHT), but_press(WIO_5S_PRESSs);
 
 // ------------------------------------------------
 // Callback Methods
 // ------------------------------------------------
-//<Button Callback !Start!>
-//<Button Callback !End!>
+// Common Button callback
+bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+{
+  // Typecast the parameters to match the GUI and element types
+  gslc_tsGui*     pGui     = (gslc_tsGui*)(pvGui);
+  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
+  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+
+  if ( eTouch == GSLC_TOUCH_UP_IN ) {
+    // From the element's ID we can determine which button was pressed.
+    switch (pElem->nId) {
+//<Button Enums !Start!>
+      case E_ELEM_NUMINPUT1:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_drvTemp);
+        break;
+      case E_ELEM_NUMINPUT2:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_ledTemp);
+        break;
+      case E_ELEM_NUMINPUT3:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_slider);
+        break;
+      case E_ELEM_NUMINPUT4:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_timeSec);
+        break;
+      case E_ELEM_NUMINPUT5:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_timeMin);
+        break;
+      case E_ELEM_NUMINPUT6:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_timeHour);
+        break;
+      case E_ELEM_NUMINPUT7:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_repSec);
+        break;
+      case E_ELEM_TOGGLE1:
+        // TODO Add code for Toggle button ON/OFF state
+        if (gslc_ElemXTogglebtnGetState(&m_gui, m_repEnable)) {
+          ;
+        }
+        break;
+      case E_ELEM_NUMINPUT8:
+        // Clicked on edit field, so show popup box and associate with this text field
+        gslc_ElemXKeyPadInputAsk(&m_gui, m_pElemKeyPadNum, E_POP_KEYPAD_NUM, m_rep);
+        break;
+
+//<Button Enums !End!>
+      default:
+        break;
+    }
+  }
+  return true;
+}
 //<Checkbox Callback !Start!>
 //<Checkbox Callback !End!>
-//<Keypad Callback !Start!>
-//<Keypad Callback !End!>
+// KeyPad Input Ready callback
+bool CbKeypad(void* pvGui, void *pvElemRef, int16_t nState, void* pvData)
+{
+  gslc_tsGui*     pGui     = (gslc_tsGui*)pvGui;
+  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
+  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+
+  // From the pvData we can get the ID element that is ready.
+  int16_t nTargetElemId = gslc_ElemXKeyPadDataTargetIdGet(pGui, pvData);
+  if (nState == XKEYPAD_CB_STATE_DONE) {
+    // User clicked on Enter to leave popup
+    // - If we have a popup active, pass the return value directly to
+    //   the corresponding value field
+    switch (nTargetElemId) {
+//<Keypad Enums !Start!>
+      case E_ELEM_NUMINPUT1:
+        gslc_ElemXKeyPadInputGet(pGui, m_drvTemp, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT2:
+        gslc_ElemXKeyPadInputGet(pGui, m_ledTemp, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT3:
+        gslc_ElemXKeyPadInputGet(pGui, m_slider, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT4:
+        gslc_ElemXKeyPadInputGet(pGui, m_timeSec, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT5:
+        gslc_ElemXKeyPadInputGet(pGui, m_timeMin, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT6:
+        gslc_ElemXKeyPadInputGet(pGui, m_timeHour, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT7:
+        gslc_ElemXKeyPadInputGet(pGui, m_repSec, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+      case E_ELEM_NUMINPUT8:
+        gslc_ElemXKeyPadInputGet(pGui, m_rep, pvData);
+        gslc_PopupHide(&m_gui);
+        break;
+
+//<Keypad Enums !End!>
+      default:
+        break;
+    }
+  } else if (nState == XKEYPAD_CB_STATE_CANCEL) {
+    // User escaped from popup, so don't update values
+    gslc_PopupHide(&m_gui);
+  }
+  return true;
+}
 //<Spinner Callback !Start!>
 //<Spinner Callback !End!>
 //<Listbox Callback !Start!>
 //<Listbox Callback !End!>
 //<Draw Callback !Start!>
 //<Draw Callback !End!>
-//<Slider Callback !Start!>
-//<Slider Callback !End!>
+
+// Callback function for when a slider's position has been updated
+bool CbSlidePos(void* pvGui,void* pvElemRef,int16_t nPos)
+{
+  gslc_tsGui*     pGui     = (gslc_tsGui*)(pvGui);
+  gslc_tsElemRef* pElemRef = (gslc_tsElemRef*)(pvElemRef);
+  gslc_tsElem*    pElem    = gslc_GetElemFromRef(pGui,pElemRef);
+  int16_t         nVal;
+
+  // From the element's ID we can determine which slider was updated.
+  switch (pElem->nId) {
+//<Slider Enums !Start!>
+    case E_ELEM_SEEKBAR1:
+      // Fetch the slider position
+      nVal = gslc_ElemXSeekbarGetPos(pGui,m_chamberFan);
+      break;
+    case E_ELEM_SEEKBAR4:
+      // Fetch the slider position
+      nVal = gslc_ElemXSeekbarGetPos(pGui,m_intensitySlider);
+      break;
+
+//<Slider Enums !End!>
+    default:
+      break;
+  }
+
+  return true;
+}
 //<Tick Callback !Start!>
 //<Tick Callback !End!>
 
+int press_duration = 100;
+
+void aPressed() {
+    Serial.println("Button a has been pressed for the given duration!");
+}
+
+void bPressed() {
+    Serial.println("Button b has been pressed for the given duration!");
+}
+
+void cPressed() {
+    Serial.println("Button c has been pressed for the given duration!");
+}
+
+void upPressed() {
+    Serial.println("Button up has been pressed for the given duration!");
+}
+
+void downPressed() {
+    Serial.println("Button down has been pressed for the given duration!");
+}
+
+void leftPressed() {
+    Serial.println("Button left has been pressed for the given duration!");
+}
+
+void rightPressed() {
+    Serial.println("Button right has been pressed for the given duration!");
+}
+
+void pressPressed() {
+    Serial.println("Button press has been pressed for the given duration!");
+}
+
 void setup()
 {
-  // ------------------------------------------------
-  // Initialize
-  // ------------------------------------------------
-  Serial.begin(9600);
-  // Wait for USB Serial 
-  //delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
-  pinMode(WIO_KEY_A,INPUT_PULLUP);
-  pinMode(WIO_KEY_B,INPUT_PULLUP);
-  gslc_InitDebug(&DebugOut);
-
+  Serial.begin();
+  
+  but_a.begin(); but_b.begin(); but_c.begin();
+  but_up.begin(); but_down.begin(); but_left.begin(); but_right.begin(); but_press.begin();
+  but_a.onPressedFor(press_duration, aPressed);
+  
   // ------------------------------------------------
   // Create graphic elements
   // ------------------------------------------------
--- src/uven2_mk2_wio_terminal_guislicer/uven2_mk2_wio_terminal_guislicer_GSLC.h
+++ src/uven2_mk2_wio_terminal_guislicer/uven2_mk2_wio_terminal_guislicer_GSLC.h
@@ -21,7 +21,12 @@
 // Include any extended elements
 //<Includes !Start!>
 // Include extended elements
+#include "elem/XKeyPad_Num.h"
 #include "elem/XProgress.h"
+#include "elem/XSeekbar.h"
+#include "elem/XTogglebtn.h"
+
+// Ensure optional features are enabled in the configuration
 //<Includes !End!>
 
 // ------------------------------------------------
@@ -45,10 +50,17 @@
 // Enumerations for pages, elements, fonts, images
 // ------------------------------------------------
 //<Enum !Start!>
-enum {E_PG_MAIN,E_PG2};
-enum {E_ELEM_PROGRESS1};
+enum {E_PG_MAIN,E_PG2,E_POP_KEYPAD_NUM};
+enum {E_ELEM_BOX1,E_ELEM_BOX2,E_ELEM_BOX3,E_ELEM_BOX5,E_ELEM_BOX6
+      ,E_ELEM_NUMINPUT1,E_ELEM_NUMINPUT2,E_ELEM_NUMINPUT3
+      ,E_ELEM_NUMINPUT4,E_ELEM_NUMINPUT5,E_ELEM_NUMINPUT6
+      ,E_ELEM_NUMINPUT7,E_ELEM_NUMINPUT8,E_ELEM_PROGRESS1
+      ,E_ELEM_PROGRESS3,E_ELEM_PROGRESS4,E_ELEM_SEEKBAR1
+      ,E_ELEM_SEEKBAR4,E_ELEM_TEXT1,E_ELEM_TEXT2,E_ELEM_TEXT3
+      ,E_ELEM_TEXT4,E_ELEM_TEXT5,E_ELEM_TEXT6,E_ELEM_TEXT7,E_ELEM_TEXT8
+      ,E_ELEM_TEXT9,E_ELEM_TOGGLE1,E_ELEM_KEYPAD_NUM};
 // Must use separate enum for fonts with MAX_FONT at end to use gslc_FontSet.
-enum {MAX_FONT};
+enum {E_BUILTIN5X8,E_FREEMONO12,E_FREEMONO9,E_FREEMONOBOLD12,MAX_FONT};
 //<Enum !End!>
 
 // ------------------------------------------------
@@ -59,9 +71,9 @@ enum {MAX_FONT};
 // Define the maximum number of elements and pages
 // ------------------------------------------------
 //<ElementDefines !Start!>
-#define MAX_PAGE                2
+#define MAX_PAGE                3
 
-#define MAX_ELEM_PG_MAIN 1 // # Elems total on page
+#define MAX_ELEM_PG_MAIN 28 // # Elems total on page
 #define MAX_ELEM_PG_MAIN_RAM MAX_ELEM_PG_MAIN // # Elems in RAM
 
 #define MAX_ELEM_PG2 0 // # Elems total on page
@@ -81,7 +93,15 @@ gslc_tsElem                     m_asPage1Elem[MAX_ELEM_PG_MAIN_RAM];
 gslc_tsElemRef                  m_asPage1ElemRef[MAX_ELEM_PG_MAIN];
 gslc_tsElem                     m_asPage2Elem[MAX_ELEM_PG2_RAM];
 gslc_tsElemRef                  m_asPage2ElemRef[MAX_ELEM_PG2];
+gslc_tsElem                     m_asKeypadNumElem[1];
+gslc_tsElemRef                  m_asKeypadNumElemRef[1];
+gslc_tsXKeyPad                  m_sKeyPadNum;
+gslc_tsXProgress                m_sXBarGauge4;
 gslc_tsXProgress                m_sXBarGauge1;
+gslc_tsXSeekbar                 m_sXSeekbar1;
+gslc_tsXProgress                m_sXBarGauge3;
+gslc_tsXSeekbar                 m_sXSeekbar4;
+gslc_tsXTogglebtn               m_asXToggle1;
 
 #define MAX_STR                 100
 
@@ -93,7 +113,21 @@ gslc_tsXProgress                m_sXBarGauge1;
 
 // Element References for direct access
 //<Extern_References !Start!>
-extern gslc_tsElemRef* m_pElemProgress1;
+extern gslc_tsElemRef* m_chamberFan;
+extern gslc_tsElemRef* m_drvTemp;
+extern gslc_tsElemRef* m_drvTempSlider;
+extern gslc_tsElemRef* m_intensitySlider;
+extern gslc_tsElemRef* m_interlock;
+extern gslc_tsElemRef* m_ledTemp;
+extern gslc_tsElemRef* m_ledTempSlider;
+extern gslc_tsElemRef* m_rep;
+extern gslc_tsElemRef* m_repEnable;
+extern gslc_tsElemRef* m_repSec;
+extern gslc_tsElemRef* m_slider;
+extern gslc_tsElemRef* m_timeHour;
+extern gslc_tsElemRef* m_timeMin;
+extern gslc_tsElemRef* m_timeSec;
+extern gslc_tsElemRef* m_pElemKeyPadNum;
 //<Extern_References !End!>
 
 // Define debug message function
@@ -124,11 +158,16 @@ void InitGUIslice_gen()
   // Load Fonts
   // ------------------------------------------------
 //<Load_Fonts !Start!>
+    if (!gslc_FontSet(&m_gui,E_BUILTIN5X8,GSLC_FONTREF_PTR,NULL,1)) { return; }
+    if (!gslc_FontSet(&m_gui,E_FREEMONO12,GSLC_FONTREF_PTR,&FreeMono12pt7b,1)) { return; }
+    if (!gslc_FontSet(&m_gui,E_FREEMONO9,GSLC_FONTREF_PTR,&FreeMono9pt7b,1)) { return; }
+    if (!gslc_FontSet(&m_gui,E_FREEMONOBOLD12,GSLC_FONTREF_PTR,&FreeMonoBold12pt7b,1)) { return; }
 //<Load_Fonts !End!>
 
 //<InitGUI !Start!>
   gslc_PageAdd(&m_gui,E_PG_MAIN,m_asPage1Elem,MAX_ELEM_PG_MAIN_RAM,m_asPage1ElemRef,MAX_ELEM_PG_MAIN);
   gslc_PageAdd(&m_gui,E_PG2,m_asPage2Elem,MAX_ELEM_PG2_RAM,m_asPage2ElemRef,MAX_ELEM_PG2);
+  gslc_PageAdd(&m_gui,E_POP_KEYPAD_NUM,m_asKeypadNumElem,1,m_asKeypadNumElemRef,1);  // KeyPad
 
   // NOTE: The current page defaults to the first page added. Here we explicitly
   //       ensure that the main page is the correct page no matter the add order.
@@ -140,15 +179,217 @@ void InitGUIslice_gen()
   // -----------------------------------
   // PAGE: E_PG_MAIN
   
+   
+  // Create E_ELEM_BOX1 box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX1,E_PG_MAIN,(gslc_tsRect){191,2,125,235});
+   
+  // Create E_ELEM_BOX2 box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX2,E_PG_MAIN,(gslc_tsRect){2,175,186,62});
+   
+  // Create E_ELEM_BOX3 box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX3,E_PG_MAIN,(gslc_tsRect){2,110,186,62});
+
+  // Create progress bar E_ELEM_PROGRESS4 
+  pElemRef = gslc_ElemXProgressCreate(&m_gui,E_ELEM_PROGRESS4,E_PG_MAIN,&m_sXBarGauge4,
+    (gslc_tsRect){2,2,186,44},0,100,0,GSLC_COL_RED,false);
+  m_interlock = pElemRef;
 
   // Create progress bar E_ELEM_PROGRESS1 
   pElemRef = gslc_ElemXProgressCreate(&m_gui,E_ELEM_PROGRESS1,E_PG_MAIN,&m_sXBarGauge1,
-    (gslc_tsRect){22,10,12,50},0,100,0,GSLC_COL_GREEN,true);
-  m_pElemProgress1 = pElemRef;
+    (gslc_tsRect){259,20,47,165},0,100,0,GSLC_COL_GREEN,true);
+  m_drvTempSlider = pElemRef;
+
+  // Create seekbar E_ELEM_SEEKBAR1 
+  pElemRef = gslc_ElemXSeekbarCreate(&m_gui,E_ELEM_SEEKBAR1,E_PG_MAIN,&m_sXSeekbar1,
+    (gslc_tsRect){10,200,175,30},0,100,0,
+    4,2,8,GSLC_COL_BLUE,GSLC_COL_GRAY,GSLC_COL_BLUE_DK2,false);
+  gslc_ElemXSeekbarSetPosFunc(&m_gui,pElemRef,&CbSlidePos);
+  gslc_ElemXSeekbarSetStyle(&m_gui,pElemRef,true,GSLC_COL_BLUE_LT4,true,GSLC_COL_GRAY,
+    0,10,GSLC_COL_GRAY);
+  m_chamberFan = pElemRef;
+  
+  // Create E_ELEM_TEXT1 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT1,E_PG_MAIN,(gslc_tsRect){268,220,32,10},
+    (char*)"DRV",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_TEXT2 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT2,E_PG_MAIN,(gslc_tsRect){10,184,119,11},
+    (char*)"Chamber Fan",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_NUMINPUT1 numeric input field
+  static char m_sInputNumber1[4] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT1,E_PG_MAIN,(gslc_tsRect){261,195,47,17},
+    (char*)m_sInputNumber1,4,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_drvTemp = pElemRef;
+
+  // Create progress bar E_ELEM_PROGRESS3 
+  pElemRef = gslc_ElemXProgressCreate(&m_gui,E_ELEM_PROGRESS3,E_PG_MAIN,&m_sXBarGauge3,
+    (gslc_tsRect){201,20,47,165},0,100,0,GSLC_COL_GREEN,true);
+  m_ledTempSlider = pElemRef;
+  
+  // Create E_ELEM_TEXT3 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT3,E_PG_MAIN,(gslc_tsRect){208,220,30,10},
+    (char*)"LED",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_NUMINPUT2 numeric input field
+  static char m_sInputNumber2[4] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT2,E_PG_MAIN,(gslc_tsRect){201,195,47,17},
+    (char*)m_sInputNumber2,4,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_ledTemp = pElemRef;
+  
+  // Create E_ELEM_TEXT4 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT4,E_PG_MAIN,(gslc_tsRect){193,5,90,10},
+    (char*)"TEMPERATURE",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_NUMINPUT3 numeric input field
+  static char m_sInputNumber3[5] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT3,E_PG_MAIN,(gslc_tsRect){120,120,61,17},
+    (char*)m_sInputNumber3,5,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_slider = pElemRef;
+  
+  // Create E_ELEM_TEXT5 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT5,E_PG_MAIN,(gslc_tsRect){30,20,124,14},
+    (char*)"INTERLOCK",0,E_FREEMONOBOLD12);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLACK);
+
+  // Create seekbar E_ELEM_SEEKBAR4 
+  pElemRef = gslc_ElemXSeekbarCreate(&m_gui,E_ELEM_SEEKBAR4,E_PG_MAIN,&m_sXSeekbar4,
+    (gslc_tsRect){10,140,175,30},0,100,20,
+    10,2,12,((gslc_tsColor){0,255,238}),GSLC_COL_GRAY,GSLC_COL_BLUE_DK2,false);
+  gslc_ElemXSeekbarSetPosFunc(&m_gui,pElemRef,&CbSlidePos);
+  gslc_ElemXSeekbarSetStyle(&m_gui,pElemRef,true,GSLC_COL_BLUE_LT4,true,GSLC_COL_GRAY,
+    4,10,GSLC_COL_GRAY);
+  m_intensitySlider = pElemRef;
+  
+  // Create E_ELEM_TEXT6 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT6,E_PG_MAIN,(gslc_tsRect){10,122,95,13},
+    (char*)"Intensity",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+   
+  // Create E_ELEM_BOX5 box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX5,E_PG_MAIN,(gslc_tsRect){2,80,186,27});
+  
+  // Create E_ELEM_TEXT7 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT7,E_PG_MAIN,(gslc_tsRect){10,87,41,10},
+    (char*)"Time",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_NUMINPUT4 numeric input field
+  static char m_sInputNumber4[3] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT4,E_PG_MAIN,(gslc_tsRect){150,85,33,17},
+    (char*)m_sInputNumber4,3,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_timeSec = pElemRef;
+  
+  // Create E_ELEM_NUMINPUT5 numeric input field
+  static char m_sInputNumber5[3] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT5,E_PG_MAIN,(gslc_tsRect){105,85,33,17},
+    (char*)m_sInputNumber5,3,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_timeMin = pElemRef;
+  
+  // Create E_ELEM_NUMINPUT6 numeric input field
+  static char m_sInputNumber6[3] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT6,E_PG_MAIN,(gslc_tsRect){60,85,33,17},
+    (char*)m_sInputNumber6,3,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_timeHour = pElemRef;
+   
+  // Create E_ELEM_BOX6 box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX6,E_PG_MAIN,(gslc_tsRect){2,49,186,27});
+  
+  // Create E_ELEM_NUMINPUT7 numeric input field
+  static char m_sInputNumber7[3] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT7,E_PG_MAIN,(gslc_tsRect){150,54,33,17},
+    (char*)m_sInputNumber7,3,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_repSec = pElemRef;
+  
+  // Create toggle button E_ELEM_TOGGLE1
+  pElemRef = gslc_ElemXTogglebtnCreate(&m_gui,E_ELEM_TOGGLE1,E_PG_MAIN,&m_asXToggle1,
+    (gslc_tsRect){5,53,35,20},GSLC_COL_GRAY,GSLC_COL_BLUE_DK2,GSLC_COL_GRAY_LT3,
+    true,false,&CbBtnCommon);
+  m_repEnable = pElemRef;
+  
+  // Create E_ELEM_NUMINPUT8 numeric input field
+  static char m_sInputNumber8[3] = "";
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_NUMINPUT8,E_PG_MAIN,(gslc_tsRect){80,54,33,17},
+    (char*)m_sInputNumber8,3,E_FREEMONO12);
+  gslc_ElemSetTxtMargin(&m_gui,pElemRef,5);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_WHITE);
+  gslc_ElemSetFrameEn(&m_gui,pElemRef,true);
+  gslc_ElemSetClickEn(&m_gui, pElemRef, true);
+  gslc_ElemSetTouchFunc(&m_gui, pElemRef, &CbBtnCommon);
+  m_rep = pElemRef;
+  
+  // Create E_ELEM_TEXT8 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT8,E_PG_MAIN,(gslc_tsRect){44,59,29,11},
+    (char*)"rep",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
+  
+  // Create E_ELEM_TEXT9 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT9,E_PG_MAIN,(gslc_tsRect){116,59,29,8},
+    (char*)"sec",0,E_FREEMONO9);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  gslc_ElemSetTxtCol(&m_gui,pElemRef,GSLC_COL_BLUE_LT3);
 
   // -----------------------------------
   // PAGE: E_PG2
   
+
+  // -----------------------------------
+  // PAGE: E_POP_KEYPAD_NUM
+  
+  static gslc_tsXKeyPadCfg_Num sCfg;
+  sCfg = gslc_ElemXKeyPadCfgInit_Num();
+  gslc_ElemXKeyPadCfgSetFloatEn_Num(&sCfg, true);
+  gslc_ElemXKeyPadCfgSetSignEn_Num(&sCfg, true);
+  m_pElemKeyPadNum = gslc_ElemXKeyPadCreate_Num(&m_gui, E_ELEM_KEYPAD_NUM, E_POP_KEYPAD_NUM,
+    &m_sKeyPadNum, 65, 80, E_BUILTIN5X8, &sCfg);
+  gslc_ElemXKeyPadValSetCb(&m_gui, m_pElemKeyPadNum, &CbKeypad);
 //<InitGUI !End!>
 
 //<Startup !Start!>
